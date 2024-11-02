0ac763a1664c4eaa21137210d5e25b8448efd5a5
BaoCoder288203 <baon6777@gmail.com>
2024-11-02 16:58:10 +0700

    Commit File App, package.json and yarn.lock

--- App-Music/App.js
+++ App-Music/App.js
@@ -1,30 +1,31 @@
-import React, { useEffect ,useState} from 'react'; // import useEffect
+import React, { useEffect, useState } from 'react'; // import useEffect
 import { NavigationContainer } from '@react-navigation/native'; // Đừng quên import NavigationContainer
 import { createNativeStackNavigator } from '@react-navigation/native-stack'; // import Stack Navigator
 import Home from './pages/Home';
 import Login from './pages/Login';
+import SongListScreen from './pages/SongListScreen';
+import MusicPlayerScreen from './pages/MusicPlayerScreen';
 import ProfileScreen from './pages/ProfileScreen';
-// import * as SplashScreen from 'expo-splash-screen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(true);
 
-    useEffect(() => {
-        // Kiểm tra trạng thái đăng nhập từ async storage hoặc logic xác thực
-        // Đặt setIsLoggedIn(true hoặc false dựa vào trạng thái xác thực)
-    }, []);
-
     return (
         <NavigationContainer>
+            {/* Uncomment the following code if you want to include Login and Home screens */}
+            {/* 
             <Stack.Navigator>
-                    <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-                    <>
-                        <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
-                        <Stack.Screen name="Home" component={Home} />
-                    </>
+                <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+                <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+            */}
+            <Stack.Navigator initialRouteName="SongList">
+                <Stack.Screen name="SongList" component={SongListScreen} options={{ title: 'Song List' }} />
+                <Stack.Screen name="MusicPlayerScreen" component={MusicPlayerScreen} options={{ title: 'Music Player' }} />
             </Stack.Navigator>
         </NavigationContainer>
     );
-};
+}
